e23225eeb2acf85d0172122830a9f4af4413faf0
online-developer <online-developer@github.com>
2019-01-01 03:26:05 -0500

    Updated pipeline

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -21,5 +21,5 @@ def test_sampling_multiple_word():
 
 def test_generate_buzz_of_at_least_five_words():
     phrase = generator.generate_buzz()
-    assert len(phrase.split()) >= 5
+    assert len(phrase.split()) >= 15
 
